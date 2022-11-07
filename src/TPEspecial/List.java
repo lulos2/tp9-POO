d2a4709451d2fae3123ffe8cd386a5fccaba6518
Lulo <lulobareiss@gmail.com>
2022-11-07 19:46:22 -0300

    added list features

--- src/TPEspecial/List.java
+++ src/TPEspecial/List.java
@@ -1,10 +1,10 @@
 package TPEspecial;
 
 
+import java.util.Comparator;
 
 public class List {
     protected Node start;
-
 
     public List() {
         this.start = null;
@@ -23,6 +23,16 @@ public class List {
         }
     }
 
+    public int listSize(){
+        int cont = 0;
+        Node aux = this.start;
+        while(aux != null) {
+            cont++;
+            aux = aux.getNext();
+        }
+       return cont;
+    }
+
 
     private void casoEspecial(){
         Node delete=this.start;
@@ -31,30 +41,27 @@ public class List {
 
     }
     public void deleteNode(int pos) {
-        int cont=1;
+        int cont = 1;
         try {
             if(pos == 1){
                this.casoEspecial();
             }
             else {
-                    Node aux = this.start;
-                    while (aux.getNext() != null && cont < pos - 1) {
-                        aux = aux.getNext();
-                        cont++;
+                Node aux = this.start;
+                while (aux.getNext() != null && cont < pos - 1) {
+                    aux = aux.getNext();
+                    cont++;
                 }
-
-                    Node delete = aux.getNext();
-                    if (delete.getNext() != null) {
-                        aux.setNext(delete.getNext());
-                        delete = null;
+                Node delete = aux.getNext();
+                if (delete.getNext() != null) {
+                    aux.setNext(delete.getNext());
+                    delete = null;
                 }
-                    else {
-                        aux.setNext(null);
-                        delete=null;
+                else {
+                    aux.setNext(null);
+                    delete=null;
                 }
             }
-
-
         }
         catch (Exception e){
             System.out.println(e);
@@ -63,10 +70,55 @@ public class List {
 
     public void print(){
         Node aux = this.start;
-        while(aux!=null) {
+        while(aux != null) {
             System.out.println(aux.getValue());
             aux = aux.getNext();
         }
     }
+
+    public void deleteNodeFor(Object element) {
+        int actualPosition = 1;
+        Node aux = this.start;
+        while (aux != null) {
+            if(aux.getValue().equals(element)) {
+                deleteNode(actualPosition);
+                actualPosition--;
+            }
+            aux = aux.getNext();
+            actualPosition++;
+        }
+    }
+
+    public int getFirstPosFrom(Object element) {
+        int actualPosition = 1;
+        Node aux = this.start;
+        while (aux != null) {
+            if(aux.getValue().equals(element)) {
+                return actualPosition;
+            }
+            aux = aux.getNext();
+            actualPosition++;
+
+        }
+        return -1;
+    }
+
+    public void orderBy(Comparator c) {
+        Node aux = this.start;
+
+        while (aux != null){
+            Node aux2= this.start;
+            while ((aux2.getNext() !=null)) {
+                if((c.compare(aux2.getValue(),aux2.getNext().getValue()))>0){
+                    Object value = aux2.getValue();
+                    aux2.setValue(aux2.getNext().getValue());
+                    aux2.getNext().setValue(value);
+                }
+                aux2 = aux2.getNext();
+            }
+            aux = aux.getNext();
+        }
+    }
+
 
 }

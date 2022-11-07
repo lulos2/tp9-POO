612d887233b6572bd03ebb6ea81a5a80cad813c5
Lulo <lulobareiss@gmail.com>
2022-11-07 19:52:35 -0300

    comparator tester

--- src/TPEspecial/Main.java
+++ src/TPEspecial/Main.java
@@ -1,16 +1,56 @@
 package TPEspecial;
 
+import TPEspecial.Comparators.ComparatorApellido;
+import TPEspecial.Comparators.ComparatorNumeroMayor;
+
+import java.util.Comparator;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("hello word");
 
         List l1=new List();
 
-        l1.insertFinal(2);
+        /*l1.insertFinal(2);
         l1.insertFinal(4);
+        l1.insertFinal(3);
         l1.insertFinal(6);
-        l1.deleteNode(1);
+        l1.insertFinal(0);
+        l1.insertFinal(6);
+        l1.insertFinal(4);
+        l1.insertFinal(1);
+        l1.insertFinal(23);
+        l1.insertFinal(3);
+        l1.deleteNodeFor(2);
+        Comparator c= new ComparatorNumeroMayor();
+        System.out.println("Lista ordenada");
+        l1.orderBy(c);*/
+
+        Alumno a1= new Alumno("Lulo","Bareiss",41690992,23);
+        Alumno a3= new Alumno("Caro","Jacquet",41148747,24);
+        Alumno a2= new Alumno("Luke","Quiroga",38178273,28);
+        a1.addInteres("Cumbia villera");
+        a1.addInteres("Ejercicio");
+        a2.addInteres("Pelear con vecino");
+        a2.addInteres("Correr");
+        a3.addInteres("Jugar al Assassin Creed");
+        a3.addInteres("Vender Galletita");
+
+        l1.insertFinal(a1);
+        l1.insertFinal(a2);
+        l1.insertFinal(a3);
+
+        System.out.println("Sin orden");
         l1.print();
+
+        Comparator c2= new ComparatorApellido(new ComparatorNumeroMayor());
+
+        System.out.println("Con orden");
+
+        l1.orderBy(c2);
+
+        l1.print();
+
     }
 }
 
@@ -26,10 +66,11 @@ public class Main {
         colecciones de Java existentes para la implementación (ArrayList, Vector, arreglos clásicos
         [], etc ). La lista vinculada debe proveer al menos a los siguientes servicios:
      -  a) Insertar un nuevo elemento en la estructura.
-        b) Eliminar un elemento de la estructura dado una posición.
-        c) Eliminar todas las ocurrencias de un elemento de la estructura dado el elemento.
-        d) Obtener la posición de la primera ocurrencia de un elemento dado.
-        e) Un mecanismo que permita recorrer uno a uno los elementos de la lista.
+     -  b) Eliminar un elemento de la estructura dado una posición.
+     -  c) Eliminar todas las ocurrencias de un elemento de la estructura dado el elemento.
+     -  d) Obtener la posición de la primera ocurrencia de un elemento dado.
+     -  e) Un mecanismo que permita recorrer uno a uno los elementos de la lista.
         f) Permitir cambiar la forma en la que se ordenan los elementos (con el subsecuente
         reordenamiento de los elementos ya almacenados.
+
 */

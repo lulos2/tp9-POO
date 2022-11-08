9915bac15fafa2e3bd17ed8454bded126fe0d763
Lulo <lulobareiss@gmail.com>
2022-11-08 15:29:53 -0300

    comparator for dni and name implemented

--- src/TPEspecial/Comparators/ComparatorDNI.java
+++ src/TPEspecial/Comparators/ComparatorDNI.java
@@ -1,2 +1,16 @@
-package TPEspecial.Comparators;public class ComparatorDNI {
+package TPEspecial.Comparators;
+
+import TPEspecial.Alumno;
+
+import java.util.Comparator;
+
+public class ComparatorDNI implements Comparator<Alumno> {
+
+    public ComparatorDNI(){
+    }
+
+    @Override
+    public int compare(Alumno a1, Alumno a2) {
+        return a1.getDni() - a2.getDni();
+    }
 }
--- src/TPEspecial/Comparators/ComparatorName.java
+++ src/TPEspecial/Comparators/ComparatorName.java
@@ -1,2 +1,25 @@
-package TPEspecial.Comparators;public class ComparatorName {
+package TPEspecial.Comparators;
+
+import TPEspecial.Alumno;
+
+import java.util.Comparator;
+
+public class ComparatorName implements Comparator<Alumno> {
+
+    private Comparator next;
+
+    public ComparatorName(Comparator next) {
+        this.next = next;
+    }
+
+    @Override
+    public int compare(Alumno o1, Alumno o2) {
+        int result = o1.getNombre().compareTo(o2.getNombre());
+        if(result != 0){
+            return result;
+        }
+        else {
+            return next.compare(o1, o2);
+        }
+    }
 }
